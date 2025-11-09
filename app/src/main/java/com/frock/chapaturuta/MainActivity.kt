e6519a751abc29bca63b8e3f4132bc8454459bc9
Fabrizio <u201914181@upc.edu.pe>
2025-11-08 23:30:10 -0500

    feat(auth): add my application hilt android app

--- app/src/main/java/com/frock/chapaturuta/MainActivity.kt
+++ app/src/main/java/com/frock/chapaturuta/MainActivity.kt
@@ -13,37 +13,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.frock.chapaturuta.core.navigation.AppNav
 import com.frock.chapaturuta.core.ui.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
                 AppNav()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
