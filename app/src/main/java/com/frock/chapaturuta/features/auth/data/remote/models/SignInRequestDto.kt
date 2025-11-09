78e8e976905c11c332e5f717cd8192d984de4b27
Fabrizio <u201914181@upc.edu.pe>
2025-11-08 23:23:16 -0500

    feat(auth): add sign in, sign up and user dto

---
+++ app/src/main/java/com/frock/chapaturuta/features/auth/data/remote/models/SignInRequestDto.kt
@@ -0,0 +1,6 @@
+package com.frock.chapaturuta.features.auth.data.remote.models
+
+data class SignInRequestDto(
+    val email:String,
+    val password:String
+)
---
+++ app/src/main/java/com/frock/chapaturuta/features/auth/data/remote/models/UserDto.kt
@@ -0,0 +1,7 @@
+package com.frock.chapaturuta.features.auth.data.remote.models
+
+data class UserDto(
+    val id:Int,
+    val email:String,
+    val token:String
+)
